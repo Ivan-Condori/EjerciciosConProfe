eda3180a7630b05809ab25926e85a706adfc8f72
Elvio Iván Condorí <elviocondori42@gmail.com>
2019-05-06 20:22:59 -0300

    Laboratorio 3 Arreglado

--- src/Laboratorio3/Entrenador.java
+++ src/Laboratorio3/Entrenador.java
@@ -32,22 +32,5 @@ public class Entrenador {
     public static int getContadorEntrenadores() {
         return contadorEntrenadores;
     }
-      
-//    private String nombre;
-//    private String tipo;
-//    public Entrenador(){ // Punto 2
-//    }
-//    public Entrenador(String nombre, String tipo){ // Punto 3
-//    this.nombre = nombre;
-//    this.tipo = tipo;
-//    }
-//    public void setEntrenador(String nombre, String tipo ){
-//        this.nombre=nombre;
-//        this.tipo=tipo;
-//    }
-//    public void indicarEntrenamiento(){ // Punto 4
-//        System.out.println("Mi nombre es "+this.nombre+ " y soy entrenador de "+this.tipo);
-//        }
 
-    
 }
--- src/Laboratorio3/Laboratorio3.java
+++ src/Laboratorio3/Laboratorio3.java
@@ -1,39 +1,28 @@
 package Laboratorio3;
 public class Laboratorio3 {
     public static void main(String[]args){ // Punto 1
-        Entrenador ent1 = new Entrenador("Iván","Futbol");      
-        Entrenador ent2 = new Entrenador("Maxi","Voley");
-        Entrenador ent3 = new Entrenador("Mauro","Tenis");
-        Entrenador ent4 = new Entrenador("Franco","Golf");
-        Entrenador ent5 = new Entrenador("Gabriel","Rugby");
+        Entrenador ent1 = new Entrenador("Iván","Futbol");  
         System.out.println("Primera Impresion: ");
-        System.out.println("Entrenador 1: "+ent1.getNombre()+"\t Tipo: "+ent1.getTipo());
-        System.out.println("Entrenador 2: "+ent2.getNombre()+"\t Tipo: "+ent2.getTipo());
-        System.out.println("Entrenador 3: "+ent3.getNombre()+"\t Tipo: "+ent3.getTipo());
-        System.out.println("Entrenador 4: "+ent4.getNombre()+"\t Tipo: "+ent4.getTipo());
-        System.out.println("Entrenador 5: "+ent5.getNombre()+"\t Tipo: "+ent5.getTipo());
+        System.out.println("Entrenador "+ent1.getContadorEntrenadores()+": "+ent1.getNombre()+"\t Tipo: "+ent1.getTipo());
+        Entrenador ent2 = new Entrenador("Maxi","Voley");
+        System.out.println("Entrenador "+ent2.getContadorEntrenadores()+": "+ent2.getNombre()+"\t Tipo: "+ent2.getTipo());
+        Entrenador ent3 = new Entrenador("Mauro","Tenis");
+        System.out.println("Entrenador "+ent3.getContadorEntrenadores()+": "+ent3.getNombre()+"\t Tipo: "+ent3.getTipo());
+        Entrenador ent4 = new Entrenador("Franco","Golf");
+        System.out.println("Entrenador "+ent4.getContadorEntrenadores()+": "+ent4.getNombre()+"\t Tipo: "+ent4.getTipo());
+        Entrenador ent5 = new Entrenador("Gabriel","Rugby");
+        System.out.println("Entrenador "+ent5.getContadorEntrenadores()+": "+ent5.getNombre()+"\t Tipo: "+ent5.getTipo());
         
-        ent1 = new Entrenador("Franco","Rugby");      
-        ent2 = new Entrenador("Maxi","Tenis");
-        ent3 = new Entrenador("Alejandro","Golf");
-        ent4 = new Entrenador("Francisco","Softbol");
-        ent5 = new Entrenador("Michael","Rugby");
+        ent1 = new Entrenador("Franco","Rugby");
         System.out.println("\nSegunda Impresion: ");
-        System.out.println("Entrenador 1: "+ent1.getNombre()+"\t Tipo: "+ent1.getTipo());
-        System.out.println("Entrenador 2: "+ent2.getNombre()+"\t Tipo: "+ent2.getTipo());
-        System.out.println("Entrenador 3: "+ent3.getNombre()+"\t Tipo: "+ent3.getTipo());
-        System.out.println("Entrenador 4: "+ent4.getNombre()+"\t Tipo: "+ent4.getTipo());
-        System.out.println("Entrenador 5: "+ent5.getNombre()+"\t Tipo: "+ent5.getTipo());
-        
-//        Entrenador entrenador1 = new Entrenador();// Punto 5.1
-//        entrenador1.setEntrenador("Francisco", "Tenis");
-//        
-//        entrenador1.indicarEntrenamiento(); // Punto 5.2
-//        System.out.println("La variable entrenador1: "+entrenador1+"\n");// Punto 5.3
-//
-//        Entrenador entrenador2 = new Entrenador( "Iván", "Fútbol"); // Punto 5.4       
-//        entrenador2.indicarEntrenamiento(); // Punto 5.5
-//        System.out.println("La variable entrenador2: "+entrenador2); // Punto 5.6            
-        
+        System.out.println("Entrenador "+ent1.getContadorEntrenadores()+": "+ent1.getNombre()+"\t Tipo: "+ent1.getTipo());
+        ent2 = new Entrenador("Maxi","Tenis");
+        System.out.println("Entrenador "+ent2.getContadorEntrenadores()+": "+ent2.getNombre()+"\t Tipo: "+ent2.getTipo());
+        ent3 = new Entrenador("Alejandro","Golf");
+        System.out.println("Entrenador "+ent3.getContadorEntrenadores()+": "+ent3.getNombre()+"\t Tipo: "+ent3.getTipo());
+        ent4 = new Entrenador("Francisco","Softbol");
+        System.out.println("Entrenador "+ent4.getContadorEntrenadores()+": "+ent4.getNombre()+"\t Tipo: "+ent4.getTipo());
+        ent5 = new Entrenador("Michael","Rugby");
+        System.out.println("Entrenador "+ent5.getContadorEntrenadores()+": "+ent5.getNombre()+"\t Tipo: "+ent5.getTipo());
     }
 }
